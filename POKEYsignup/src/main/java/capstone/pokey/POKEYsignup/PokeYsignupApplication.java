f9c2e4c8c29860c76c5f821f5d397a490ee42155
SeungTaeGit <129585999+SeungTaeGit@users.noreply.github.com>
2023-06-03 18:46:58 +0900

    Add files via upload

---
+++ POKEYsignup/src/main/java/capstone/pokey/POKEYsignup/PokeYsignupApplication.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeYsignupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeYsignupApplication.class, args);
+	}
+
+}
---
+++ POKEYsignup/src/test/java/capstone/pokey/POKEYsignup/PokeYsignupApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYsignup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeYsignupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
